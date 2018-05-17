90a174eec4bbbef6b3d96f26f9ab21826a4cba89
suragch <studymongolian@gmail.com>
2018-05-17 08:19:09 +0800

    update to mongol-library 1.5

--- app/src/main/java/net/studymongolian/todochimee/MyInputMethodService.java
+++ app/src/main/java/net/studymongolian/todochimee/MyInputMethodService.java
@@ -1,6 +1,7 @@
 package net.studymongolian.todochimee;
 
 import android.inputmethodservice.InputMethodService;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
@@ -8,14 +9,20 @@ import android.view.inputmethod.InputMethodManager;
 
 import net.studymongolian.mongollibrary.ImeContainer;
 
-public class MyInputMethodService extends InputMethodService implements ImeContainer.OnSystemImeListener {
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+public class MyInputMethodService extends InputMethodService
+        implements ImeContainer.OnSystemImeListener, ImeContainer.DataSource {
 
     @Override
     public View onCreateInputView() {
         LayoutInflater inflater = getLayoutInflater();
         ImeContainer jianpan = (ImeContainer) inflater.inflate(R.layout.jianpan_yangshi, null, false);
-        jianpan.showSystemKeyboardsOption("ᠰᡇᡊᡎᡇᡍᡇ"); // 长按键盘键可以切换到别的系统输入法
+        jianpan.showSystemKeyboardsOption(getString(R.string.show_system_keyboards_option)); // 长按键盘键可以切换到别的系统输入法
         jianpan.setOnSystemImeListener(this);
+        jianpan.setDataSource(this);
         return jianpan;
     }
 
@@ -31,5 +38,22 @@ public class MyInputMethodService extends InputMethodService implements ImeConta
         InputMethodManager im = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
         if (im == null) return;
         im.showInputMethodPicker();
+    }
+
+    // ImeContainer.DataSource的方法
+
+    @Override
+    public void onRequestWordsStartingWith(String text) {
+        Log.i(TAG, "onRequestWordsStartingWith: ");
+    }
+
+    @Override
+    public void onRequestWordsFollowing(String word) {
+        Log.i(TAG, "onRequestWordsFollowing: ");
+    }
+
+    @Override
+    public void onCandidateLongClick(int position, String text) {
+        Log.i(TAG, "onCandidateLongClick: ");
     }
 }
